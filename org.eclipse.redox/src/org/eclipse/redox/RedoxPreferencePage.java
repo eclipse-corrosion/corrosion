72c2267482aef5b52014ff61514e7ada9aa45f15
Lucas Bullen <lbullen@redhat.com>
2017-12-14 15:13:30 -0500

    Issue #8 Install Rustup From Within Eclipse
    - Added button in preferences to install rustup and cargo
    
    Signed-off-by: Lucas Bullen <lbullen@redhat.com>

--- org.eclipse.redox/src/org/eclipse/redox/RedoxPreferencePage.java
+++ org.eclipse.redox/src/org/eclipse/redox/RedoxPreferencePage.java
@@ -15,25 +15,33 @@ package org.eclipse.redox;
 import static org.eclipse.swt.events.SelectionListener.widgetSelectedAdapter;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.ICoreRunnable;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.program.Program;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Link;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.osgi.framework.Bundle;
 
 public class RedoxPreferencePage extends PreferencePage implements IWorkbenchPreferencePage {
 	public static final String PAGE_ID = "org.eclipse.redox.preferencePage";
@@ -79,14 +87,9 @@ public class RedoxPreferencePage extends PreferencePage implements IWorkbenchPre
 
 		rustupRadioButton = new Button(container, SWT.RADIO);
 		rustupRadioButton.setText("Use Rustup");
+		rustupRadioButton.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 2, 1));
 		rustupRadioButton.addSelectionListener(widgetSelectedAdapter(e -> {
 			setRadioSelection(0);
-		}));
-		Link installLink = new Link(container, SWT.NONE);
-		installLink.setText("(Rustup can be downloaded from: <a href=\"https://rustup.rs/\">https://rustup.rs</a>)");
-		installLink.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false));
-		installLink.addSelectionListener(widgetSelectedAdapter(e -> {
-			Program.launch("https://rustup.rs/");
 		}));
 
 		createRustupPart(container);
@@ -130,28 +133,30 @@ public class RedoxPreferencePage extends PreferencePage implements IWorkbenchPre
 	}
 
 	private boolean isCommandPathsValid() {
+		String error = "";
 		if ((rustupPathText.getText().isEmpty() || cargoPathText.getText().isEmpty())) {
-			setErrorMessage("Rustup and Cargo paths cannot be empty");
-			return false;
+			error = "Rustup and Cargo paths cannot be empty";
+		} else {
+			File rustup = new File(rustupPathText.getText());
+			File cargo = new File(cargoPathText.getText());
+			if (!rustup.exists() || !rustup.isFile()) {
+				error = "Input a valid `rustup` command path";
+			} else if (!rustup.canExecute()) {
+				error = "Inputted `rustup` command is not executable";
+			} else if (!cargo.exists() || !cargo.isFile()) {
+				error = "Input a valid `cargo` command path";
+			} else if (!cargo.canExecute()) {
+				error = "Inputted `cargo` command is not executable";
+			}
 		}
-		File rustup = new File(rustupPathText.getText());
-		if (!rustup.exists() || !rustup.isFile()) {
-			setErrorMessage("Input a valid `rustup` command path");
-			return false;
-		} else if (!rustup.canExecute()) {
-			setErrorMessage("Inputted `rustup` command is not executable");
-			return false;
+
+		if (error.isEmpty()) {
+			setErrorMessage(null);
+		} else {
+			setErrorMessage(error);
 		}
-		File cargo = new File(cargoPathText.getText());
-		if (!cargo.exists() || !cargo.isFile()) {
-			setErrorMessage("Input a valid `cargo` command path");
-			return false;
-		} else if (!cargo.canExecute()) {
-			setErrorMessage("Inputted `cargo` command is not executable");
-			return false;
-		}
-		setErrorMessage(null);
-		return true;
+		setInstallRequired(!error.isEmpty());
+		return error.isEmpty();
 	}
 
 	private boolean isRustupSectionValid() {
@@ -197,7 +202,10 @@ public class RedoxPreferencePage extends PreferencePage implements IWorkbenchPre
 				.indexOf(store.getDefaultString(RedoxPreferenceInitializer.toolchainTypePreference));
 		otherIdText.setText(store.getDefaultString(RedoxPreferenceInitializer.toolchainIdPreference));
 		setToolchainSelection(toolchainIndex);
-		setDefaultPathsSelection(store.getDefaultBoolean(RedoxPreferenceInitializer.defaultPathsPreference));
+		useDefaultPathsCheckbox
+				.setSelection(store.getDefaultBoolean(RedoxPreferenceInitializer.defaultPathsPreference));
+		rustupPathText.setText(store.getDefaultString(RedoxPreferenceInitializer.rustupPathPreference));
+		cargoPathText.setText(store.getDefaultString(RedoxPreferenceInitializer.cargoPathPreference));
 		rlsPathText.setText(store.getDefaultString(RedoxPreferenceInitializer.rlsPathPreference));
 		sysrootPathText.setText(store.getDefaultString(RedoxPreferenceInitializer.sysrootPathPreference));
 		super.performDefaults();
@@ -280,6 +288,8 @@ public class RedoxPreferencePage extends PreferencePage implements IWorkbenchPre
 		}
 	}
 
+	private Button installButton;
+
 	private Button useDefaultPathsCheckbox;
 
 	private Label rustupLabel;
@@ -299,8 +309,8 @@ public class RedoxPreferencePage extends PreferencePage implements IWorkbenchPre
 		GridData buttonGridData = new GridData(SWT.LEFT, SWT.CENTER, false, false);
 
 		useDefaultPathsCheckbox = new Button(parent, SWT.CHECK);
-		useDefaultPathsCheckbox.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 3, 1));
-		useDefaultPathsCheckbox.setText("Use default command paths");
+		useDefaultPathsCheckbox.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 2, 1));
+		useDefaultPathsCheckbox.setText("Use default paths (rustup and cargo required for most features)");
 		useDefaultPathsCheckbox.addSelectionListener(widgetSelectedAdapter(e -> {
 			setDefaultPathsEnabled(!useDefaultPathsCheckbox.getSelection());
 			if (useDefaultPathsCheckbox.getSelection()) {
@@ -308,6 +318,11 @@ public class RedoxPreferencePage extends PreferencePage implements IWorkbenchPre
 				cargoPathText.setText(store.getDefaultString(RedoxPreferenceInitializer.cargoPathPreference));
 			}
 			setValid(isPageValid());
+		}));
+		installButton = new Button(parent, SWT.NONE);
+		installButton.setLayoutData(buttonGridData);
+		installButton.addSelectionListener(widgetSelectedAdapter(e -> {
+			installCommands();
 		}));
 
 		rustupLabel = new Label(parent, SWT.NONE);
@@ -351,6 +366,47 @@ public class RedoxPreferencePage extends PreferencePage implements IWorkbenchPre
 				cargoPathText.setText(path);
 			}
 		}));
+	}
+
+	private void installCommands() {
+		installButton.setText("Installling");
+		installButton.setEnabled(false);
+		Job.create("Installing Rustup and Cargo", (ICoreRunnable) monitor -> {
+			try {
+				Bundle bundle = Platform.getBundle("org.eclipse.redox");
+				URL fileURL = bundle.getEntry("scripts/rustup.sh");
+				File file = new File(FileLocator.resolve(fileURL).toURI());
+				String[] command = new String[] { "/bin/sh", file.getAbsolutePath() };
+				ProcessBuilder builder = new ProcessBuilder(command);
+				Process process = builder.start();
+				if (process.waitFor() == 0) {
+					RedoxPreferenceInitializer initializer = new RedoxPreferenceInitializer();
+					initializer.initializeDefaultPreferences();
+					Display.getDefault().asyncExec(() -> {
+						setInstallRequired(false);
+						performDefaults();
+						setValid(isPageValid());
+					});
+					return;
+				}
+			} catch (InterruptedException | URISyntaxException | IOException e) {
+				// will be caught with dialog
+			}
+			setInstallRequired(true);
+			Display.getDefault().asyncExec(() -> {
+				MessageDialog.openError(getShell(), "Cannot install Rust and Cargo",
+						"We were unable to perform the install, you can do so manually by going to doc.crates.io");
+			});
+		}).schedule();
+	}
+
+	private void setInstallRequired(Boolean required) {
+		if (required) {
+			installButton.setText("Install");
+		} else {
+			installButton.setText("Installed");
+		}
+		installButton.setEnabled(required);
 	}
 
 	private Label rustupToolchainLabel;
