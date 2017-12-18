184233e397287c86260c4ca37eeba53321c87763
Lucas Bullen <lbullen@redhat.com>
2017-12-18 16:58:06 -0500

    Issue #10 Send didChangeConfiguration when Preferences are updated
    - Able to send updated to settings on updating preferences
    - Will add more updates when more customization in the preferences come
    available
    
    Signed-off-by: Lucas Bullen <lbullen@redhat.com>

--- org.eclipse.redox/src/org/eclipse/redox/RustManager.java
+++ org.eclipse.redox/src/org/eclipse/redox/RustManager.java
@@ -16,15 +16,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.eclipse.core.runtime.ICoreRunnable;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.lsp4e.LanguageServiceAccessor;
+import org.eclipse.lsp4e.LanguageServiceAccessor.LSPDocumentInfo;
+import org.eclipse.lsp4j.DidChangeConfigurationParams;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorReference;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.texteditor.ITextEditor;
 
+//TODO: investigate solution not requiring breaking restrictions
+@SuppressWarnings("restriction")
 public class RustManager {
 	private static final IPreferenceStore STORE = RedoxPlugin.getDefault().getPreferenceStore();
 
@@ -91,7 +106,47 @@ public class RustManager {
 				showToolchainSelectionError("Unable to add required components, please select a different toolchain");
 				return;
 			}
+			Map<String, String> updatedSettings = new HashMap<>();
+			updatedSettings.put("target", toolchainId);
+
+			sendDidChangeConfigurationsMessage(updatedSettings);
 		}).schedule();
+	}
+
+	private static void sendDidChangeConfigurationsMessage(Map<String, String> updatedSettings) {
+		DidChangeConfigurationParams params = new DidChangeConfigurationParams();
+		params.setSettings(updatedSettings);
+		LSPDocumentInfo info = infoFromOpenEditors();
+		if (info != null) {
+			info.getInitializedLanguageClient()
+					.thenAccept(languageServer -> languageServer.getWorkspaceService().didChangeConfiguration(params));
+		}
+	}
+
+	private static LSPDocumentInfo infoFromOpenEditors() {
+		for (IWorkbenchWindow window : PlatformUI.getWorkbench().getWorkbenchWindows()) {
+			for (IWorkbenchPage page : window.getPages()) {
+				for (IEditorReference editor : page.getEditorReferences()) {
+					IEditorInput input;
+					try {
+						input = editor.getEditorInput();
+					} catch (PartInitException e) {
+						continue;
+					}
+					if (input.getName().endsWith(".rs") && editor.getEditor(false) instanceof ITextEditor) {
+						IDocument document = (((ITextEditor) editor.getEditor(false)).getDocumentProvider())
+								.getDocument(input);
+						Collection<LSPDocumentInfo> infos = LanguageServiceAccessor.getLSPDocumentInfosFor(document,
+								capabilities -> Boolean.TRUE.equals(capabilities.getReferencesProvider()));
+						if (infos.isEmpty()) {
+							continue;
+						}
+						return infos.iterator().next();
+					}
+				}
+			}
+		}
+		return null;
 	}
 
 	private static void showToolchainSelectionError(String message) {
