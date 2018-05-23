9e627f6faad5fe82f1e7078165cb0f90ff44c343
Lucas Bullen <lbullen@redhat.com>
2018-05-23 09:24:45 -0400

    Issue #74: Can't launch rust project
     - Fixed a copy paste error
     - error dialogs no longer hold the UI thread
     - Updated tests to correctly test launching
    
    Signed-off-by: Lucas Bullen <lbullen@redhat.com>

--- org.eclipse.corrosion.tests/src/org/eclipse/corrosion/tests/TestRunConfiguration.java
+++ org.eclipse.corrosion.tests/src/org/eclipse/corrosion/tests/TestRunConfiguration.java
@@ -13,6 +13,7 @@
 package org.eclipse.corrosion.tests;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -20,6 +21,7 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.corrosion.run.CargoRunDelegate;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
@@ -27,7 +29,9 @@ import org.eclipse.debug.core.ILaunchConfigurationType;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.core.ILaunchManager;
 import org.eclipse.debug.core.model.IProcess;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.tests.harness.util.DisplayHelper;
 import org.junit.Test;
 
@@ -35,39 +39,53 @@ public class TestRunConfiguration extends AbstractCorrosionTest {
 
 	@Test
 	public void testBasicRun() throws Exception {
+		CargoRunDelegate delegate = new CargoRunDelegate();
 		IProject project = getProject("basic");
-		ILaunchConfiguration launchConfiguration = createLaunchConfiguration(project, "launch");
-		confirmLaunchConfiguration(launchConfiguration, 0);
+		delegate.launch(new StructuredSelection(project), "run");
+		new DisplayHelper() {
+			@Override
+			protected boolean condition() {
+				return DebugPlugin.getDefault().getLaunchManager().getProcesses().length != 0 || errorPopupExists();
+			}
+		}.waitForCondition(Display.getCurrent(), 15000);
+		assertFalse(errorPopupExists());
+		assertTrue(DebugPlugin.getDefault().getLaunchManager().getProcesses().length != 0);
+		for (IProcess process : DebugPlugin.getDefault().getLaunchManager().getProcesses()) {
+			if (process.getLabel().equals("cargo run")) {
+				while (!process.isTerminated()) {
+					Thread.sleep(50);
+				}
+				assertEquals(0, process.getExitValue());
+				return;
+			}
+		}
 	}
 
 	// TODO: Unable to test multiple failing runs do to bug
 	// computer crashes if multiple popups are made one after another
 	// a popup is made if the configuration is unable to run
-	// @Test
-	// public void testFailOnFakeProjectName() throws Exception {
-	// IProject project = getProject("basic");
-	// ILaunchConfigurationWorkingCopy launchConfiguration =
-	// createLaunchConfiguration(project, "launch");
-	// launchConfiguration.setAttribute("PROJECT", "fakeProjectName");
-	// confirmLaunchConfiguration(launchConfiguration, 1);
-	// }
-	//
-	// @Test
-	// public void testFailOnDeletedProject() throws Exception {
-	// IProject project = getProject("basic");
-	// ILaunchConfigurationWorkingCopy launchConfiguration =
-	// createLaunchConfiguration(project, "launch");
-	// project.delete(true, new NullProgressMonitor());
-	// confirmLaunchConfiguration(launchConfiguration, 1);
-	// }
-	//
-	// @Test
-	// public void testFailOnNonCargoProject() throws Exception {
-	// IProject project = getProject("not_cargo");
-	// ILaunchConfigurationWorkingCopy launchConfiguration =
-	// createLaunchConfiguration(project, "launch");
-	// confirmLaunchConfiguration(launchConfiguration, 1);
-	// }
+	@Test
+	public void testFailOnFakeProjectName() throws Exception {
+		IProject project = getProject("basic");
+		ILaunchConfigurationWorkingCopy launchConfiguration = createLaunchConfiguration(project, "launch");
+		launchConfiguration.setAttribute("PROJECT", "fakeProjectName");
+		confirmErrorPopup(launchConfiguration);
+	}
+
+	@Test
+	public void testFailOnDeletedProject() throws Exception {
+		IProject project = getProject("basic");
+		ILaunchConfigurationWorkingCopy launchConfiguration = createLaunchConfiguration(project, "launch");
+		project.delete(true, new NullProgressMonitor());
+		confirmErrorPopup(launchConfiguration);
+	}
+
+	@Test
+	public void testFailOnNonCargoProject() throws Exception {
+		IProject project = getProject("not_cargo");
+		ILaunchConfigurationWorkingCopy launchConfiguration = createLaunchConfiguration(project, "launch");
+		confirmErrorPopup(launchConfiguration);
+	}
 
 	@Test
 	public void testTranslateVariablesInBuildCommand() throws Exception {
@@ -109,26 +127,24 @@ public class TestRunConfiguration extends AbstractCorrosionTest {
 		return wc;
 	}
 
-	private void confirmLaunchConfiguration(ILaunchConfiguration configuration, int expectedExitValue)
-			throws Exception {
-		ILaunch launch = configuration.launch(ILaunchManager.RUN_MODE, new NullProgressMonitor());
-
+	private void confirmErrorPopup(ILaunchConfiguration configuration) throws Exception {
+		configuration.launch(ILaunchManager.RUN_MODE, new NullProgressMonitor());
 		new DisplayHelper() {
 			@Override
 			protected boolean condition() {
-				return launch.getProcesses().length != 0;
+				return errorPopupExists();
 			}
 		}.waitForCondition(Display.getDefault(), 15000);
 
-		for (IProcess process : launch.getProcesses()) {
-			if (process.getLabel().equals("cargo run")) {
-				while (!process.isTerminated()) {
-					Thread.sleep(50);
-				}
-				assertEquals(expectedExitValue, process.getExitValue());
-				return;
+		assertTrue(errorPopupExists());
+	}
+
+	private boolean errorPopupExists() {
+		for (Shell shell : Display.getDefault().getShells()) {
+			if (shell.getText().equals("Unable to Launch")) {
+				return true;
 			}
 		}
-		assertEquals(expectedExitValue, 1);
+		return false;
 	}
 }
