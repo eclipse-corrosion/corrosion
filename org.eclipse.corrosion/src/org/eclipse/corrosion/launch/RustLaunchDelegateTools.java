8e247ac221cd7b0ba16f2ad19826eca4596d46a8
Lucas Bullen <lbullen@redhat.com>
2018-05-24 12:29:41 -0400

    Issue #73 - Better factorize various LaunchConfig classes
     - Moved duplicate code to helper class
     - Unable to make an abstract class with content due to
    RustDebugDelegate already extending GDBLaunchDelegate
    
    Signed-off-by: Lucas Bullen <lbullen@redhat.com>

---
+++ org.eclipse.corrosion/src/org/eclipse/corrosion/launch/RustLaunchDelegateTools.java
@@ -0,0 +1,135 @@
+/*********************************************************************
+ * Copyright (c) 2018 Red Hat Inc. and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *  Lucas Bullen (Red Hat Inc.) - Initial implementation
+ *******************************************************************************/
+package org.eclipse.corrosion.launch;
+
+import java.util.Iterator;
+
+import org.eclipse.cdt.debug.core.ICDTLaunchConfigurationConstants;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.corrosion.CorrosionPlugin;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationType;
+import org.eclipse.debug.core.ILaunchManager;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.PlatformUI;
+
+public class RustLaunchDelegateTools {
+
+	/**
+	 * Returns the first resource from a structured selection
+	 *
+	 * @param selection
+	 * @return First element in the selection or null if nothing is selected
+	 */
+	public static IResource firstResourceFromSelection(ISelection selection) {
+		if (selection instanceof IStructuredSelection) {
+			Iterator<?> selectionIterator = ((IStructuredSelection) selection).iterator();
+			while (selectionIterator.hasNext()) {
+				Object element = selectionIterator.next();
+				if (element instanceof IResource) {
+					return (IResource) element;
+				} else if (element instanceof IAdaptable) {
+					return ((IAdaptable) element).getAdapter(IResource.class);
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Returns the resource that is the input of the given editor
+	 *
+	 * @param editor
+	 * @return The resource from the editor
+	 */
+	public static IResource resourceFromEditor(IEditorPart editor) {
+		if (editor == null) {
+			return null;
+		}
+		IEditorInput input = editor.getEditorInput();
+		return input.getAdapter(IResource.class);
+	}
+
+	/**
+	 * Launches the given launch configuration
+	 *
+	 * @param launchConfig
+	 * @param mode
+	 */
+	public static void launch(ILaunchConfiguration launchConfig, String mode) {
+		try {
+			if (launchConfig != null) {
+				launchConfig.launch(mode, new NullProgressMonitor());
+			}
+		} catch (CoreException e) {
+			CorrosionPlugin.logError(e);
+		}
+	}
+
+	/**
+	 * Finds or creates a new launch configuration that matches the given search
+	 * terms
+	 *
+	 * @param mode
+	 * @param resource
+	 * @param LaunchConfigurationType
+	 * @return The matching launch configuration or a new launch configuration
+	 *         working copy or null if unable to make a new one
+	 */
+	public static ILaunchConfiguration getLaunchConfiguration(String mode, IResource resource,
+			String LaunchConfigurationType) {
+		ILaunchManager launchManager = DebugPlugin.getDefault().getLaunchManager();
+		ILaunchConfigurationType configType = launchManager.getLaunchConfigurationType(LaunchConfigurationType);
+		try {
+			ILaunchConfiguration[] launchConfigurations = launchManager.getLaunchConfigurations(configType);
+			final String projectName = resource.getProject().getName();
+
+			for (ILaunchConfiguration iLaunchConfiguration : launchConfigurations) {
+				if (iLaunchConfiguration.getAttribute(ICDTLaunchConfigurationConstants.ATTR_PROJECT_NAME, "") //$NON-NLS-1$
+						.equals(projectName)) {
+					return iLaunchConfiguration;
+				}
+			}
+			String configName = launchManager.generateLaunchConfigurationName(projectName);
+			return configType.newInstance(null, configName);
+		} catch (CoreException e) {
+			CorrosionPlugin.logError(e);
+		}
+		return null;
+	}
+
+	/**
+	 * Opens a non blocking error dialog
+	 * 
+	 * @param title
+	 * @param message
+	 */
+	public static void openError(String title, String message) {
+		Display.getDefault().asyncExec(() -> {
+			MessageDialog dialog = new MessageDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
+					title, null, message, MessageDialog.ERROR, 0, IDialogConstants.OK_LABEL);
+			dialog.setBlockOnOpen(false);
+			dialog.open();
+		});
+	}
+}
--- org.eclipse.corrosion/src/org/eclipse/corrosion/test/CargoTestDelegate.java
+++ org.eclipse.corrosion/src/org/eclipse/corrosion/test/CargoTestDelegate.java
@@ -14,7 +14,6 @@ package org.eclipse.corrosion.test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
@@ -23,31 +22,22 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.variables.IStringVariableManager;
 import org.eclipse.core.variables.VariablesPlugin;
-import org.eclipse.corrosion.CorrosionPlugin;
 import org.eclipse.corrosion.Messages;
 import org.eclipse.corrosion.cargo.core.CargoTools;
+import org.eclipse.corrosion.launch.RustLaunchDelegateTools;
 import org.eclipse.corrosion.run.CargoRunDelegate;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
-import org.eclipse.debug.core.ILaunchConfigurationType;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
-import org.eclipse.debug.core.ILaunchManager;
 import org.eclipse.debug.core.model.IProcess;
 import org.eclipse.debug.core.model.LaunchConfigurationDelegate;
 import org.eclipse.debug.ui.ILaunchShortcut;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.PlatformUI;
 
 public class CargoTestDelegate extends LaunchConfigurationDelegate implements ILaunchShortcut {
 
@@ -56,49 +46,18 @@ public class CargoTestDelegate extends LaunchConfigurationDelegate implements IL
 	public static final String TEST_ARGUMENTS_ATTRIBUTE = CargoRunDelegate.RUN_ARGUMENTS_ATTRIBUTE;
 	public static final String TEST_NAME_ATTRIBUTE = "TEST_NAME"; //$NON-NLS-1$
 
-	@Override public void launch(ISelection selection, String mode) {
-
-		if (selection instanceof IStructuredSelection) {
-			Iterator<?> selectionIterator = ((IStructuredSelection) selection).iterator();
-			while (selectionIterator.hasNext()) {
-				Object element = selectionIterator.next();
-				IResource resource = null;
-				if (element instanceof IResource) {
-					resource = (IResource) element;
-				} else if (element instanceof IAdaptable) {
-					resource = ((IAdaptable) element).getAdapter(IResource.class);
-				}
-
-				if (resource != null) {
-					try {
-						ILaunchConfiguration launchConfig = getLaunchConfiguration(mode, resource);
-						if (launchConfig != null) {
-							launchConfig.launch(mode, new NullProgressMonitor());
-						}
-					} catch (CoreException e) {
-						CorrosionPlugin.logError(e);
-					}
-					return;
-				}
-			}
-		}
-		Display.getDefault().asyncExec(() -> {
-			MessageDialog.openError(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), Messages.CargoRunDelegate_unableToLaunch, Messages.CargoRunDelegate_unableToLaunch);
-		});
+	@Override
+	public void launch(ISelection selection, String mode) {
+		ILaunchConfiguration launchConfig = getLaunchConfiguration(mode,
+				RustLaunchDelegateTools.firstResourceFromSelection(selection));
+		RustLaunchDelegateTools.launch(launchConfig, mode);
 	}
 
-	@Override public void launch(IEditorPart editor, String mode) {
-		IEditorInput input = editor.getEditorInput();
-		IFile file = input.getAdapter(IFile.class);
-
-		try {
-			ILaunchConfiguration launchConfig = getLaunchConfiguration(mode, file);
-			if (launchConfig != null) {
-				launchConfig.launch(mode, new NullProgressMonitor());
-			}
-		} catch (CoreException e) {
-			CorrosionPlugin.logError(e);
-		}
+	@Override
+	public void launch(IEditorPart editor, String mode) {
+		ILaunchConfiguration launchConfig = getLaunchConfiguration(mode,
+				RustLaunchDelegateTools.resourceFromEditor(editor));
+		RustLaunchDelegateTools.launch(launchConfig, mode);
 	}
 
 	@Override public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor) throws CoreException {
@@ -115,16 +74,14 @@ public class CargoTestDelegate extends LaunchConfigurationDelegate implements IL
 			project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
 		}
 		if (project == null || !project.exists()) {
-			Display.getDefault().asyncExec(() -> {
-				MessageDialog.openError(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), Messages.CargoRunDelegate_unableToLaunch, Messages.CargoRunDelegate_unableToFindProject);
-			});
+			RustLaunchDelegateTools.openError(Messages.CargoRunDelegate_unableToLaunch,
+					Messages.CargoRunDelegate_unableToFindProject);
 			return;
 		}
 		IFile cargoManifest = project.getFile("Cargo.toml"); //$NON-NLS-1$
 		if (!cargoManifest.exists()) {
-			Display.getDefault().asyncExec(() -> {
-				MessageDialog.openError(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), Messages.CargoRunDelegate_unableToLaunch, Messages.CargoRunDelegate_unableToFindToml);
-			});
+			RustLaunchDelegateTools.openError(Messages.CargoRunDelegate_unableToLaunch,
+					Messages.CargoRunDelegate_unableToFindToml);
 			return;
 		}
 		IStringVariableManager manager = VariablesPlugin.getDefault().getStringVariableManager();
@@ -153,32 +110,18 @@ public class CargoTestDelegate extends LaunchConfigurationDelegate implements IL
 				IProcess process = DebugPlugin.newProcess(launch, p, "cargo test"); //$NON-NLS-1$
 				process.setAttribute(IProcess.ATTR_CMDLINE, String.join(" ", cmdLine)); //$NON-NLS-1$
 			} catch (CoreException e) {
-				Display.getDefault().asyncExec(() -> {
-					MessageDialog.openError(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), Messages.CargoRunDelegate_unableToLaunch, e.getLocalizedMessage());
-				});
+				RustLaunchDelegateTools.openError(Messages.CargoRunDelegate_unableToLaunch, e.getLocalizedMessage());
 			}
 		});
 	}
 
 	private ILaunchConfiguration getLaunchConfiguration(String mode, IResource resource) {
-		ILaunchManager launchManager = DebugPlugin.getDefault().getLaunchManager();
-		ILaunchConfigurationType configType = launchManager.getLaunchConfigurationType("org.eclipse.corrosion.test.CargoTestDelegate"); //$NON-NLS-1$
-		try {
-			ILaunchConfiguration[] launchConfigurations = launchManager.getLaunchConfigurations(configType);
-			final String projectName = resource.getProject().getName();
-
-			for (ILaunchConfiguration iLaunchConfiguration : launchConfigurations) {
-				if (iLaunchConfiguration.getAttribute(PROJECT_ATTRIBUTE, "").equals(projectName)) { //$NON-NLS-1$
-					return iLaunchConfiguration;
-				}
-			}
-			String configName = launchManager.generateLaunchConfigurationName(projectName);
-			ILaunchConfigurationWorkingCopy wc = configType.newInstance(null, configName);
-			wc.setAttribute(PROJECT_ATTRIBUTE, projectName); // $NON-NLS-3$
-			return wc;
-		} catch (CoreException e) {
-			CorrosionPlugin.logError(e);
+		ILaunchConfiguration launchConfiguration = RustLaunchDelegateTools.getLaunchConfiguration(mode, resource,
+				"org.eclipse.corrosion.test.CargoTestDelegate"); //$NON-NLS-1$
+		if (launchConfiguration instanceof ILaunchConfigurationWorkingCopy) {
+			ILaunchConfigurationWorkingCopy wc = (ILaunchConfigurationWorkingCopy) launchConfiguration;
+			wc.setAttribute(PROJECT_ATTRIBUTE, resource.getProject().getName());
 		}
-		return null;
+		return launchConfiguration;
 	}
 }
