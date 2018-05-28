08883bdee56442135f8efd2bd8f1af0620450a39
Lucas Bullen <lbullen@redhat.com>
2018-05-28 14:20:25 -0400

    Issue #58: Progress report for Installing
     - Add a progress dialog to the installation process
     - Use the output of the toolchain downloading commands to give the user
    more info
     - Fix typo in messages
     - Abstracted running commands to a new Job class
    
    Signed-off-by: Lucas Bullen <lbullen@redhat.com>

---
+++ org.eclipse.corrosion/src/org/eclipse/corrosion/CommandJob.java
@@ -0,0 +1,78 @@
+/*********************************************************************
+ * Copyright (c) 2018 Red Hat Inc. and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *  Lucas Bullen (Red Hat Inc.) - Initial implementation
+ *******************************************************************************/
+package org.eclipse.corrosion;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.concurrent.CompletableFuture;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.SubMonitor;
+import org.eclipse.core.runtime.jobs.Job;
+
+public class CommandJob extends Job {
+	private Process process;
+	private String[] command;
+	private String progressMessage;
+	private String errorTitle;
+	private String errorMessage;
+	private int expectedWork;
+
+	public CommandJob(String[] command, String progressMessage, String errorTitle, String errorMessage,
+			int expectedWork) {
+		super(progressMessage);
+		this.command = command;
+		this.progressMessage = progressMessage;
+		this.errorTitle = errorTitle;
+		this.errorMessage = errorMessage;
+		this.expectedWork = expectedWork;
+	}
+
+	@Override
+	protected IStatus run(IProgressMonitor monitor) {
+		SubMonitor subMonitor = SubMonitor.convert(monitor, expectedWork);
+		try {
+			subMonitor.beginTask(progressMessage, expectedWork);
+			process = new ProcessBuilder(command).start();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+			CompletableFuture.runAsync(() -> {
+				reader.lines().forEachOrdered(line -> {
+					subMonitor.subTask(line);
+					if (expectedWork > 0) {
+						subMonitor.worked(1);
+					}
+				});
+			});
+			if (process.waitFor() != 0) {
+				if (!subMonitor.isCanceled()) {
+					CorrosionPlugin.showError(errorTitle, errorMessage);
+				}
+				return Status.CANCEL_STATUS;
+			}
+			return Status.OK_STATUS;
+		} catch (IOException | InterruptedException e) {
+			CorrosionPlugin.showError(errorTitle, errorMessage, e);
+			return Status.CANCEL_STATUS;
+		}
+	}
+
+	@Override
+	protected void canceling() {
+		if (process != null) {
+			process.destroyForcibly();
+		}
+	}
+}
