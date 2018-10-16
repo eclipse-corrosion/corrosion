b94fdd0addd928dce375f50787bbcab4ad490d9c
Alexander Kurtakov <akurtako@redhat.com>
2018-10-16 13:44:48 +0300

    Add Arguments tab to debug tab group.
    Fixes #151 .

--- org.eclipse.corrosion/src/org/eclipse/corrosion/debug/RustDebugTabGroup.java
+++ org.eclipse.corrosion/src/org/eclipse/corrosion/debug/RustDebugTabGroup.java
@@ -15,6 +15,7 @@ package org.eclipse.corrosion.debug;
 import org.eclipse.cdt.debug.core.ICDTLaunchConfigurationConstants;
 import org.eclipse.cdt.dsf.gdb.IGDBLaunchConfigurationConstants;
 import org.eclipse.cdt.dsf.gdb.internal.ui.launching.LocalApplicationCDebuggerTab;
+import org.eclipse.cdt.launch.ui.CArgumentsTab;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
 import org.eclipse.debug.ui.CommonTab;
@@ -26,13 +27,16 @@ import org.eclipse.debug.ui.sourcelookup.SourceLookupTab;
 @SuppressWarnings("restriction")
 public class RustDebugTabGroup extends AbstractLaunchConfigurationTabGroup {
 
-	@Override public void createTabs(ILaunchConfigurationDialog arg0, String arg1) {
-		setTabs(new ILaunchConfigurationTab[] { new RustDebugTab(), new EnvironmentTab(), new RustLocalApplicationCDebuggerTab(), new SourceLookupTab(), new CommonTab() });
+	@Override
+	public void createTabs(ILaunchConfigurationDialog arg0, String arg1) {
+		setTabs(new ILaunchConfigurationTab[] { new RustDebugTab(), new CArgumentsTab(), new EnvironmentTab(),
+				new RustLocalApplicationCDebuggerTab(), new SourceLookupTab(), new CommonTab() });
 	}
 
 	protected class RustLocalApplicationCDebuggerTab extends LocalApplicationCDebuggerTab {
 
-		@Override public void setDefaults(ILaunchConfigurationWorkingCopy config) {
+		@Override
+		public void setDefaults(ILaunchConfigurationWorkingCopy config) {
 			super.setDefaults(config);
 			config.setAttribute(ICDTLaunchConfigurationConstants.ATTR_DEBUGGER_STOP_AT_MAIN, false);
 			config.setAttribute(IGDBLaunchConfigurationConstants.ATTR_DEBUG_NAME, "rust-gdb"); //$NON-NLS-1$
