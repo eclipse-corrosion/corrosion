24e48f8bfadb869fe20fad67a679b4b5de2d90c2
Alexander Kurtakov <akurtako@redhat.com>
2018-12-18 14:46:16 +0200

    Fix unused import.
    And enable save actions to automate these things.

--- org.eclipse.corrosion.tests/src/org/eclipse/corrosion/tests/TestLSPIntegration.java
+++ org.eclipse.corrosion.tests/src/org/eclipse/corrosion/tests/TestLSPIntegration.java
@@ -1,5 +1,5 @@
 /*********************************************************************
- * Copyright (c) 2017 Red Hat Inc. and others.
+ * Copyright (c) 2017, 2018 Red Hat Inc. and others.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -13,7 +13,6 @@
  *******************************************************************************/
 package org.eclipse.corrosion.tests;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
