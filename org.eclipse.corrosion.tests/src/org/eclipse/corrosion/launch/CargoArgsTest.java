ad7b8bdb8fa17ff28193ed69be4f81b03dee8199
Alexander Kurtakov <akurtako@redhat.com>
2021-09-30 11:08:31 +0300

    CargoArgs#fromAllArguments(String...) instead of String[]
    Simplify tests based on that.
    Other cleanups to tests while at them.

--- org.eclipse.corrosion.tests/src/org/eclipse/corrosion/launch/CargoArgsTest.java
+++ org.eclipse.corrosion.tests/src/org/eclipse/corrosion/launch/CargoArgsTest.java
@@ -1,5 +1,5 @@
 /*********************************************************************
- * Copyright (c) 2019 Fraunhofer FOKUS and others.
+ * Copyright (c) 2019, 2021 Fraunhofer FOKUS and others.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -21,64 +21,60 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
-public class CargoArgsTest {
+class CargoArgsTest {
 
 	private static final String SEPARATOR = "--";
 
 	@Test
-	public void testOnlyCommand() {
+	void testOnlyCommand() {
 		String command = "fmt";
-		String[] input = { command };
-		CargoArgs result = CargoArgs.fromAllArguments(input);
+		CargoArgs result = CargoArgs.fromAllArguments(command);
 		assertEquals(command, result.command);
 		assertTrue(result.arguments.isEmpty());
 		assertTrue(result.options.isEmpty());
 	}
 
 	@Test
-	public void testOnlyCommandAndSeparator() {
+	void testOnlyCommandAndSeparator() {
 		String command = "fmt";
-		String[] input = { command, SEPARATOR };
-		CargoArgs result = CargoArgs.fromAllArguments(input);
+		CargoArgs result = CargoArgs.fromAllArguments(command, SEPARATOR);
 		assertEquals(command, result.command);
 		assertTrue(result.arguments.isEmpty());
 		assertTrue(result.options.isEmpty());
 	}
 
 	@Test
-	public void testOnlyCommandAndOptions() {
+	void testOnlyCommandAndOptions() {
 		String command = "build";
 		String opt1 = "--all";
 		String opt2 = "--release";
-		String[] input = { command, opt1, opt2 };
-		CargoArgs result = CargoArgs.fromAllArguments(input);
+		CargoArgs result = CargoArgs.fromAllArguments(command, opt1, opt2);
 		assertEquals(command, result.command);
 		assertEquals(Arrays.asList(opt1, opt2), result.options);
 		assertTrue(result.arguments.isEmpty());
 	}
 
 	@Test
-	public void testOnlyCommandAndOptionsAndSeparator() {
+	void testOnlyCommandAndOptionsAndSeparator() {
 		String command = "build";
 		String opt1 = "--all";
 		String opt2 = "--release";
-		String[] input = { command, opt1, opt2, SEPARATOR };
-		CargoArgs result = CargoArgs.fromAllArguments(input);
+		CargoArgs result = CargoArgs.fromAllArguments(command, opt1, opt2, SEPARATOR);
 		assertEquals(command, result.command);
 		assertEquals(Arrays.asList(opt1, opt2), result.options);
 		assertTrue(result.arguments.isEmpty());
 	}
 
 	@Test
-	public void testOnlyCommandAndArguments() {
+	void testOnlyCommandAndArguments() {
 		String command = "build";
-		List<String> arguments = Arrays.asList("--nocapture", "my_test");
+		List<String> arguments = List.of("--nocapture", "my_test");
 
 		List<String> all_arguments = new ArrayList<>();
 		all_arguments.add(command);
 		all_arguments.add(SEPARATOR);
 		all_arguments.addAll(arguments);
-		String[] input = all_arguments.toArray(new String[] {});
+		String[] input = all_arguments.toArray(String[]::new);
 
 		CargoArgs result = CargoArgs.fromAllArguments(input);
 
@@ -88,17 +84,17 @@ public class CargoArgsTest {
 	}
 
 	@Test
-	public void testOnlyCommandAndOptionsAndArguments() {
+	void testOnlyCommandAndOptionsAndArguments() {
 		String command = "build";
-		List<String> options = Arrays.asList("--all", "--release");
-		List<String> arguments = Arrays.asList("--nocapture", "my_test");
+		List<String> options = List.of("--all", "--release");
+		List<String> arguments = List.of("--nocapture", "my_test");
 
 		List<String> all_arguments = new ArrayList<>();
 		all_arguments.add(command);
 		all_arguments.addAll(options);
 		all_arguments.add(SEPARATOR);
 		all_arguments.addAll(arguments);
-		String[] input = all_arguments.toArray(new String[] {});
+		String[] input = all_arguments.toArray(String[]::new);
 
 		CargoArgs result = CargoArgs.fromAllArguments(input);
 
@@ -108,17 +104,17 @@ public class CargoArgsTest {
 	}
 
 	@Test
-	public void testOnlyCommandAndOptionsAndArgumentsIncludingSeparator() {
+	void testOnlyCommandAndOptionsAndArgumentsIncludingSeparator() {
 		String command = "build";
-		List<String> options = Arrays.asList("--all", "--release");
-		List<String> arguments = Arrays.asList("--nocapture", SEPARATOR, "my_test");
+		List<String> options = List.of("--all", "--release");
+		List<String> arguments = List.of("--nocapture", SEPARATOR, "my_test");
 
 		List<String> all_arguments = new ArrayList<>();
 		all_arguments.add(command);
 		all_arguments.addAll(options);
 		all_arguments.add(SEPARATOR);
 		all_arguments.addAll(arguments);
-		String[] input = all_arguments.toArray(new String[] {});
+		String[] input = all_arguments.toArray(String[]::new);
 
 		CargoArgs result = CargoArgs.fromAllArguments(input);
 
